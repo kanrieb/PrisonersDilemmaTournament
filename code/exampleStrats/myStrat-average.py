0ce0243a8d9f04beec94ef22c2e61ef5554aa022
Kathryn Rieb <kanrieb@gmail.com>
2021-05-20 18:31:46 -0700

    Linear weighting

--- code/exampleStrats/myStrat-average.py
+++ code/exampleStrats/myStrat-average.py
@@ -1,24 +1,25 @@
-#start out with 10 cooperations
+#start out as a cooperation
 #begin analysis of opponents response
-#if average response is cooperation/staying silent (1): also cooperate
-#if average response is betrayal (0): also betray
+#if majority response is cooperation/staying silent (1): also cooperate
+#if majority response is betrayal (0): also betray
+#uses linear weighting (more recent turns are weighted as more important)
 
 def strategy(history, memory):
     choice = 1
 
-    coop_count = 0
-    defect_count= 0
 
     if history.shape[1] > 1: 
+        coop_count = 0
+        defect_count= 0
+
         for x in range(history.shape[1]):
             prev = history[1,x]
             if prev == 1:
-                coop_count += 1
+                coop_count += x
             elif prev == 0:
-                defect_count += 1
+                defect_count += x
         
         if defect_count > coop_count:
             choice = 0
 
     return choice, None
-
